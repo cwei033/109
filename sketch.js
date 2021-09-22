a341507517d3507b7b73dd22f6108be9b7c09846
cwei033 <60452220+cwei033@users.noreply.github.com>
2021-09-22 13:35:05 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let bg;
 let bgRotate = 0;
 let rSpeed = 0.2;
+// let w = windowWidth;
+// let h = windowHeight;
 
 function preload() {
   bg = loadImage('assets/bgstripe.JPG');
@@ -9,8 +11,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // background(bg);
+  createCanvas(1920, 1080);
+  // if (windowHeight > 3000) {
+  //   h = 3000
+  // }
   textAlign(CENTER);
   imageMode(CENTER);
   angleMode(DEGREES);
@@ -19,8 +23,12 @@ function setup() {
 
 function draw() {
   drawRotatebg(bgRotate);
-  bgRotate += rSpeed
-  image(tent, width / 2, height - 450, 1368, 900);
+  bgRotate += rSpeed;
+
+  push();
+  translate(width/2, height);
+  image(tent, 0, -450, width - 600, height - 180);
+  pop();
 }
 
 function drawRotatebg(rotation) {
