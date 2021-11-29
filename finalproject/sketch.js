fd6dbe721a6f526643e1073146cfebe928194aa1
cwei033 <60452220+cwei033@users.noreply.github.com>
2021-11-28 22:19:57 -0800

    finalproject

---
+++ finalproject/sketch.js
@@ -0,0 +1,21 @@
+
+
+function preload() {
+
+
+}
+
+function setup() {
+  cnv = createCanvas(windowWidth, windowHeight);
+  if (windowHeight > windowWidth) {
+    height = windowWidth * 0.7;
+  }
+  textAlign(CENTER);
+  imageMode(CENTER);
+  angleMode(DEGREES);
+
+}
+
+function draw() {
+  
+}
