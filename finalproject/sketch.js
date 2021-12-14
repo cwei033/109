f37a05a08900ddc7e64af1cbfe63a0826de09df0
cwei033 <60452220+cwei033@users.noreply.github.com>
2021-12-14 08:20:22 -0800

    Update sketch.js

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -1,10 +1,15 @@
 var xoff1 = 0;
 var xoff2 = 1000; //two xoffs so that you're pulling values from two different points in time so that the x and y coordinates are not the same
-var inc = 0.01; //change how smooth the graph is; smaller the number the smoother it is
+var inc = 0.1; //change how smooth the graph is; smaller the number the smoother it is
 var start = 0; //make the starting point of the graph a variable so that you can change it so that the graph moves rather than stays stagnant
 let static;
 let desert;
-let state = 'staticmode'
+let state = 'flowmode';
+var scl = 20;
+var cols, rows;
+var zoff = 0;
+var particles = [];
+var flowfield;
 
 function preload() {
   static = loadSound('assets/static.wav');
@@ -20,12 +25,21 @@ function setup() {
   imageMode(CENTER);
   //angleMode(DEGREES);
 
+  cols = floor(width / scl); //floor makes sure no decimals
+  rows = floor(height / scl);
+
   pixelDensity(0.5);
+
+  for (var i = 0; i < 400; i++) {
+    particles[i] = new Particle();
+  }
+
+  flowfield = new Array(cols * rows);
+  background(255);
 
 }
 
 function draw() {
-  background(255);
 
   if (state == 'staticmode') {
     staticdetail();
@@ -55,5 +69,30 @@ function staticdetail() {
 }
 
 function flowdetail() {
-  
+  desert.loop();
+  desert.setVolume(0.05);
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = (x + y * cols);
+      var angle = noise(xoff, yoff, zoff) * TWO_PI;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(0.5);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+
+    }
+    yoff += inc;
+
+    zoff += 0.0001;
+  }
+
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
 }
